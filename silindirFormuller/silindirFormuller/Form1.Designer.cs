f55221cfcfeadc70b8bb835a23b48193a20c5a5e
ttarikyuksel <89916905+ttarikyuksel@users.noreply.github.com>
2021-09-29 23:52:24 +0300

    silindirUygulama
    kullanıcı tarafından girilen değerler ile silindirin formüllerin sonucunu veren program

---
+++ silindirFormuller/silindirFormuller/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace silindirFormuller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtyukseklik = new System.Windows.Forms.TextBox();
+            this.txtyarıcap = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btntemizle = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txthacim = new System.Windows.Forms.TextBox();
+            this.txtyuzeyalani = new System.Windows.Forms.TextBox();
+            this.txttabanalani = new System.Windows.Forms.TextBox();
+            this.txtyanalalani = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::silindirFormuller.Properties.Resources.silindirin_hacmi;
+            this.pictureBox1.Location = new System.Drawing.Point(244, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(243, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "h = Yükseklik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(243, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "r = Yarıçap";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "h =";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(12, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "r =";
+            // 
+            // txtyukseklik
+            // 
+            this.txtyukseklik.Location = new System.Drawing.Point(52, 14);
+            this.txtyukseklik.Name = "txtyukseklik";
+            this.txtyukseklik.Size = new System.Drawing.Size(100, 20);
+            this.txtyukseklik.TabIndex = 5;
+            // 
+            // txtyarıcap
+            // 
+            this.txtyarıcap.Location = new System.Drawing.Point(52, 40);
+            this.txtyarıcap.Name = "txtyarıcap";
+            this.txtyarıcap.Size = new System.Drawing.Size(100, 20);
+            this.txtyarıcap.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btntemizle
+            // 
+            this.btntemizle.Location = new System.Drawing.Point(87, 83);
+            this.btntemizle.Name = "btntemizle";
+            this.btntemizle.Size = new System.Drawing.Size(65, 23);
+            this.btntemizle.TabIndex = 8;
+            this.btntemizle.Text = "TEMİZLE";
+            this.btntemizle.UseVisualStyleBackColor = true;
+            this.btntemizle.Click += new System.EventHandler(this.btntemizle_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(49, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Hacim =";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(5, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(118, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Yüzey Alanı =";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(5, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Taban Alanı =";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(13, 240);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(111, 20);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Yanal Alan =";
+            // 
+            // txthacim
+            // 
+            this.txthacim.Location = new System.Drawing.Point(129, 143);
+            this.txthacim.Name = "txthacim";
+            this.txthacim.Size = new System.Drawing.Size(100, 20);
+            this.txthacim.TabIndex = 13;
+            // 
+            // txtyuzeyalani
+            // 
+            this.txtyuzeyalani.Location = new System.Drawing.Point(129, 172);
+            this.txtyuzeyalani.Name = "txtyuzeyalani";
+            this.txtyuzeyalani.Size = new System.Drawing.Size(100, 20);
+            this.txtyuzeyalani.TabIndex = 14;
+            // 
+            // txttabanalani
+            // 
+            this.txttabanalani.Location = new System.Drawing.Point(129, 204);
+            this.txttabanalani.Name = "txttabanalani";
+            this.txttabanalani.Size = new System.Drawing.Size(100, 20);
+            this.txttabanalani.TabIndex = 15;
+            // 
+            // txtyanalalani
+            // 
+            this.txtyanalalani.Location = new System.Drawing.Point(129, 240);
+            this.txtyanalalani.Name = "txtyanalalani";
+            this.txtyanalalani.Size = new System.Drawing.Size(100, 20);
+            this.txtyanalalani.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 269);
+            this.Controls.Add(this.txtyanalalani);
+            this.Controls.Add(this.txttabanalani);
+            this.Controls.Add(this.txtyuzeyalani);
+            this.Controls.Add(this.txthacim);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btntemizle);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtyarıcap);
+            this.Controls.Add(this.txtyukseklik);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Silindir ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtyukseklik;
+        private System.Windows.Forms.TextBox txtyarıcap;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btntemizle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txthacim;
+        private System.Windows.Forms.TextBox txtyuzeyalani;
+        private System.Windows.Forms.TextBox txttabanalani;
+        private System.Windows.Forms.TextBox txtyanalalani;
+    }
+}
+
---
+++ silindirFormuller/silindirFormuller/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace silindirFormuller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region değişkenler
+
+            float yukseklik = Convert.ToSingle(txtyukseklik.Text);
+
+            float yaricap = Convert.ToSingle(txtyarıcap.Text);
+
+            #endregion
+
+            #region hacim
+
+            float hacim = yaricap * yaricap * yukseklik * 3.14f;
+
+            txthacim.Text = Convert.ToString(hacim);
+
+            #endregion
+
+            #region tabanAlani
+
+            float tabanalani = yaricap * yaricap * 3.14f;
+
+            txttabanalani.Text = Convert.ToString(tabanalani);
+
+            #endregion
+
+            #region yanalAlani
+
+            float yanalAlan = 2 * yaricap * yukseklik * 3.14f;
+
+            txtyanalalani.Text = Convert.ToString(yanalAlan);
+
+            #endregion
+
+            #region yuzeyalani
+
+            float yuzeyalani = 2 * tabanalani + yanalAlan;
+
+            txtyuzeyalani.Text = Convert.ToString(yuzeyalani);
+
+            #endregion
+
+        }
+        private void btntemizle_Click(object sender, EventArgs e)
+        #region temizle
+
+        {
+            txthacim.Clear();
+            txttabanalani.Clear();
+            txtyanalalani.Clear();
+            txtyarıcap.Clear();
+            txtyukseklik.Clear();
+            txtyuzeyalani.Clear();
+
+        }
+        #endregion
+
+    }
+}
